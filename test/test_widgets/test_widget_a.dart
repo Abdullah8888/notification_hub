3d468f1757c44d21f76e8d611c00aa4fb9df8b58
= <abdullahijimoh3.ja@gmail.com>
2024-01-04 02:15:42 +0100

    Update unit test

--- test/test_widgets/test_widget_a.dart
+++ test/test_widgets/test_widget_a.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../mock_notification_hub.dart';
+
 class TestWidgetA extends StatefulWidget {
   const TestWidgetA({super.key});
 
@@ -8,25 +10,40 @@ class TestWidgetA extends StatefulWidget {
 }
 
 class _TestWidgetAState extends State<TestWidgetA> {
-  // late String textContent;
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   print("lalalala");
-  //   MockNotificationHub.instance
-  //       .addSubscriber(this, notificationName: "ChannelOne", onData: (event) {
-  //     setState(() {
-  //       textContent = '$event';
-  //     });
-  //   }, onDone: (message) {
-  //     debugPrint("$message");
-  //   }, onError: (error) {
-  //     debugPrint(error.toString());
-  //   });
-  // }
+  late String textContent;
+  @override
+  void initState() {
+    super.initState();
+    print("lalalala");
+    MockNotificationHub.instance.addSubscriber(this,
+        notificationChannel: "ChannelOne", onData: (event) {
+      setState(() {
+        textContent = '$event';
+      });
+    }, onDone: (message) {
+      debugPrint("$message");
+    }, onError: (error) {
+      debugPrint(error.toString());
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
+  }
+}
+
+class TestObjectOne {
+  late String textContent;
+  TestObjectOne(this.textContent);
+
+  void start() {
+    MockNotificationHub.instance.addSubscriber(this,
+        notificationChannel: "ChannelOne",
+        onData: (event) {}, onDone: (message) {
+      debugPrint("$message");
+    }, onError: (error) {
+      debugPrint(error.toString());
+    });
   }
 }
